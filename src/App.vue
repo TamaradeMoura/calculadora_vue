2fde589e912f58d9161d5817d4a42056f63454d6
TamaradeMoura <tamara_moura_13@hotmail.com>
2023-09-04 20:44:57 -0300

    commit final

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,101 @@
 <script setup>
-import { reactive } from 'vue';
+import { reactive, ref } from 'vue';
 
 const calculo = reactive({
   num1: '',
   num2: '',
 })
 
-var resultado = 0
+const resultado = ref(0);
+const operacao = ref('');
 
-switch(calculo){
-  case '+':
-    resultado = num1 + num2
-    break
-    case '-':
-    resultado = num1 - num2
-    break
-    case '*':
-    resultado = num1 * num2
-    break
-    case '/':
-    resultado = num1 / num2
-    break
+function calcular() {
+  const num1 = parseFloat(calculo.num1);
+  const num2 = parseFloat(calculo.num2);
+
+  if (isNaN(num1) || isNaN(num2)) {
+    alert('Digite dois valores válidos');
+    return;
+  }
+
+  switch (operacao.value) {
+    case 'somar':
+      resultado.value = num1 + num2;
+      break;
+    case 'subtrair':
+      resultado.value = num1 - num2;
+      break;
+    case 'multi':
+      resultado.value = num1 * num2;
+      break;
+    case 'dividir':
+      if (num2 === 0) {
+        alert('Não é possível dividir por zero');
+        return;
+      }
+      resultado.value = num1 / num2;
+      break;
     default:
-    alert('Digite dois valores')
-    break
+      alert('Selecione uma operação');
+      break;
+  }
 }
-
-
-
-
 </script>
 
 <template>
-<div>
-  <h3>Calculadora Vue</h3>
-  <ol class="mat">
-    <li>
-      <input v-model.number="num1" placeholder="Digite o primeiro valor">
-      <input v-model.number="num2" placeholder="Digite o segundo valor">
-      <select  @change="evento => calculo.calculo = evento.target.value" name="seletor" id="seletor">
-        <option value="seletor">selecionar</option>
-        <option value="somar">somar</option>
-        <option value="subtrair">subtrair</option>
-        <option value="multi">multiplicar</option>
-        <option value="dividir">dividir</option>
-      </select>
-      <p>Resultado:</p>
-    </li>
-  </ol>
-</div>
-
+  <div class="container">
+    <header class="titulo">
+      <h3>Calculadora VUE</h3>
+      <img src="./img/calc.png" alt="calculadora">
+    </header>
+    <ol class="mat">
+      <li class="lista">
+        <input v-model.number="calculo.num1" placeholder="Digite o primeiro valor">
+        <input v-model.number="calculo.num2" placeholder="Digite o segundo valor">
+        <select v-model="operacao" name="seletor" id="seletor">
+          <option value="">selecionar</option>
+          <option value="somar">somar</option>
+          <option value="subtrair">subtrair</option>
+          <option value="multi">multiplicar</option>
+          <option value="dividir">dividir</option>
+        </select>
+        <button @click="calcular">Calcular</button>
+        <p class="result">Resultado: {{ resultado }}</p>
+      </li>
+    </ol>
+  </div>
 </template>
+
+
 <style scoped>
+.container {
+  max-width: 960px;
+  width: 100%;
+  margin: 0 auto;
+}
+.titulo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgb(73, 240, 204);
+}
+
+img {
+  max-width: 40px;
+  position: relative;
+  left: 2%;
+}
 .mat {
-    list-style: none;
-  }
+  list-style: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.result {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-decoration:overline;
+  font-weight: bold;
+}
 </style>
